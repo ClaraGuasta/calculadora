604709b07ac24701d6bd05ddaca2ea5009e5613a
ClaraGuasta <cmguastavino@hotmail.com>
2022-02-16 01:09:54 -0300

    calculadora

---
+++ calculadora/index.js
@@ -0,0 +1,12 @@
+const sumar = require("./sumar")
+//console.log(sumar(285,190))
+
+const restar = require("./restar")
+//console.log(restar(2,2))
+
+const multiplicar = require("./multiplicar")
+console.log(multiplicar(2,23))
+
+const dividir = require("./dividir")
+//console.log(sumar(2,0))
+
---
+++ calculadora/sumar.js
@@ -0,0 +1,12 @@
+function sumar(a,b){
+    return a + b
+ }
+
+module.exports = sumar
+
+// function clave (unConjunto){
+//     const reversed = unConjunto.reverse();
+//     let todoJunto = reversed.join("");
+//     return todoJunto;
+// }
+// console.log(clave(["e", "v", "a", "l", "c"]));
